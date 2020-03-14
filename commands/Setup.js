d8a8158babf78e4cfb5fc33e53e69115a4b62b3b
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-14 12:49:04 +0100

    update nick name
    update nick name

--- commands/Setup.js
+++ commands/Setup.js
@@ -1,7 +1,11 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    message.guild.members.get(bot.user.id).setNickname("NEWNAMEHERE")
+    var botname = args.join(" ");
+ 
+    // Kijk na als er een idee is meegegeven.
+    if (!botname) return message.channel.send("<prefix>setup (New bot name)");
+    message.guild.members.get(bot.user.id).setNickname(botname)
 }
 
 module.exports.help = {
