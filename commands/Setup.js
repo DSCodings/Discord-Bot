9999de52a2411c5794d76eb88693e662ad606c89
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-14 12:43:32 +0100

    test
    test

--- commands/Setup.js
+++ commands/Setup.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    msg.guild.members.get(bot.user.id).setNickname("NEWNAMEHERE")
+    message.guild.members.get(bot.user.id).setNickname("NEWNAMEHERE")
 }
 
 module.exports.help = {
