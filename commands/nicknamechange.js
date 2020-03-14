880be065a083f1e3018067ace350b7423aab833c
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-14 14:39:32 +0100

    nickname
    update nickname

---
+++ commands/nicknamechange.js
@@ -0,0 +1,34 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    var changenameUser = message.guild.member(message.mentions.users.first() || message.guild.members(arguments[0]));
+
+    if (!changenameUser) return message.channel.send("use was not found");
+
+
+    var nickname = args.join(" ").slice(22);
+ 
+    // Kijk na als er een idee is meegegeven.
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permission for that.");
+    if (!nickname) return message.channel.send("<prefix>nicknamechange (New nickname)");
+    message.guild.members.get(bot.user.id).setNickname(nickname);
+
+    var nicknameEmbed =  new discord.RichEmbed()
+      .setDescription("nick name successful changed")
+      .setColor("#29e53f")
+      .setThumbnail(message.guild.iconURL)
+      .addField("nickname successful changed to:", nickname)
+      .addField("More commands", "Do <prefix>help for more commands")
+      .setTimestamp()
+      .setFooter('Mady by jan0de0man', message.guild.iconURL);
+
+
+    return message.channel.send(nicknameEmbed);
+
+
+}
+
+module.exports.help = {
+    name: "nicknamechange",
+    description: "for change the nick name af a player"
+}
