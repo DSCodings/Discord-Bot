2988f1a3ec35aab3b35f37f20904718e3e6929f9
Daan Swinters <daan.swinters@gmail.com>
2020-03-15 12:58:59 +0100

    Create update.js
    update.js created

---
+++ commands/update.js
@@ -0,0 +1,32 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+     
+            var text = "**Terminal bot** \n\n **__Update Log__** \n -setup added, For setup the bot \n -nickmanechange added \n update log added";
+      
+           // message.author.send(text);
+      
+            //return message.channel.send("We sent you a DM with information.");
+
+            message.author.send(text).then(() => {
+ 
+                message.channel.send("We sent you a DM with information! :mailbox_with_mail:");
+         
+            }).catch(() => {
+         
+                message.channel.send("Your private messages are turned off, you have not received any help, pls turn in on.");
+         
+            });
+      
+          
+            
+         return message.channel.send("error");
+      
+      
+          
+}
+
+module.exports.help = {
+    name: "update",
+    description: "for see the update logs."
+}
