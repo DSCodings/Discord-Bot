c44fad39d0539470e6185f384d84e5294c0a889a
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-17 17:05:35 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -5,6 +5,7 @@ module.exports.run = async(bot, message, args) => {
     var Userroll = message.guild.member(message.mentions.users.first() || message.guild.member(arguments[0]));
     if(!Userroll) return message.reply("Couldn't find that user!");
     var roleName = args.join(" ").slice(22);
+    console.log(roleName)
     if(!roleName) return message.channel.send("<prefix>rank (Name) (roll)");
 
     let Grole = message.guild.roles.find("name", roleName);
