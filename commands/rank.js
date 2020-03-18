634061a1a4327e8abf5c7a84fbc84ece8f036b66
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-18 19:28:19 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -23,7 +23,7 @@ module.exports.run = async(bot, message, args) => {
     
     if(!Grole) return message.reply("Couldn't find that role.");
 
-    Userroll.roles.add(Grole).catch(message.reply("Can't give that roll!"));
+    Userroll.addRole(Grole).catch(message.reply("Can't give that roll!"));
   
 
     //Filtering the guild members only keeping those with the role
