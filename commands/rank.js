5b2e090177802694ff654b167220985ca141fd94
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-18 17:29:45 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -8,13 +8,12 @@ module.exports.run = async(bot, message, args) => {
     console.log(roleName);
     if(!roleName) return message.channel.send("<prefix>rank (Name) (roll)");
 
-    //let Grole = member.roles.cache.some(role => role.name === roleName);
+    let Grole = member.roles.cache.some(role => role.name === roleName);
     let Grole = message.guild.members.filter(member => { 
-        return member.roles.find("name", `${roleName}`);
-    }).map(member => {
-        return member.user.username;
+        return member.roles.find("name", roleName);
     })
     //("name", roleName);
+    //let Grole = message.guild.roles.find(role => role.name === roleName);
     console.log(message.guild.roleName);
     console.log(message.guild.roles.name);
     console.log(message.guild.roles.first.name);
