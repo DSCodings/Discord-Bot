dadd45feb7d8a665a0609f4784db5e905e1240fc
Daan Swinters <daan.swinters@gmail.com>
2020-03-19 12:40:49 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,6 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
     var Icon = message.guild.iconURL;
+    var Userroll = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
     
     // if (!user.roles) {
     // user.roles = [ ];
@@ -9,20 +10,35 @@ module.exports.run = async(bot, message, args) => {
     
     
     
-    var serverEmbed =  new discord.RichEmbed()
-      .setDescription("Userinfo")
-      .setColor("#29e53f")
-      .setThumbnail(Icon)
-      .addField("Bot name", bot.user.username)
-      .addField("you are joined on", message.member.joinedAt)
-      .addField('Roles:', user.roles ? user.roles.map(r => `${r}`).join(' | ') : "", true)
-      .setFooter('Mady by jan0de0man', message.guild.iconURL);
+    let user;
+if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+} else {
+    user = message.guild.author;
+}
+
+const member = message.guild.member(user);
+
+const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(message.author.avatarURL)
+    .addField(`${user.tag}`, `${user}`, true)
+    .addField("ID:", `${user.id}`, true)
+    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
+    .addField("Status:", `${user.presence.status}`, true)
+    .addField("In Server", message.guild.name, true)
+    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+    .addField("Bot:", `${user.bot}`, true)
+    .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
+    .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
+    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
 
 
-    return message.channel.send(serverEmbed);
+    return message.channel.send(embed);
 }
 
 module.exports.help = {
     name: "userinfo",
-    description: "for test the bot"
+    description: "for see the info from the user"
 }
