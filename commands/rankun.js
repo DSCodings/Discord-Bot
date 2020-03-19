8b61bb78213c211454083bd2c3a6dae175757cf1
Daan Swinters <daan.swinters@gmail.com>
2020-03-19 11:29:54 +0100

    Create rankun.js

---
+++ commands/rankun.js
@@ -0,0 +1,53 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("You do not have access to this command");
+    var Userroll = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
+    if(!Userroll) return message.reply("Couldn't find that user!");
+    var roleName = args.join(" ").slice(22);
+    console.log(roleName);
+    if(!roleName) return message.channel.send("<prefix>rank (Name) (roll)");
+
+    
+    // let Grole = message.guild.members.filter(member => { 
+    //     return member.roles.find(role => role.name === roleName);
+    // }).map(member => {
+    //     return member.user.username;
+    // })
+    //("name", roleName);
+    //let Grole = message.guild.roles.find(role => role.name === roleName);
+    //const Grole = message.guild.roles.find( role => { return role.name == roleName });
+    let Grole = message.mentions.roles.first();
+    // console.log(message.guild.roleName);
+    // console.log(message.guild.roles.name);
+    // console.log(message.guild.roles.first.name);
+    // console.log(Grole);
+    
+    if(!Grole) return message.reply("Couldn't find that role.");
+
+    Userroll.removeRole(Grole);
+
+    //Filtering the guild members only keeping those with the role
+    //Then mapping the filtered array to their usernames
+    // let membersWithRole = message.guild.members.filter(member => { 
+    //     return member.roles.find("name", roleName);
+    // }).map(member => {
+    //     return member.user.username;
+    // })
+
+    let rolembed = new discord.RichEmbed()
+        .setTitle(`**${Userroll} heeft succesvol de rol ${roleName}**`)
+        //.setDescription(`Users with the ${roleName} role`)
+        .setColor("0xFFFF")
+        .setTimestamp()
+        .setFooter('Mady by jan0de0man', message.guild.iconURL);
+
+
+    message.channel.send(rolembed);
+    return
+}
+
+module.exports.help = {
+    name: "unrank",
+    description: "for give a rank(only for owner)"
+}
