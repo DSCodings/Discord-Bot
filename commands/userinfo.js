b17fd3610be246dfbcac2539d95fc79a04831d6c
Daan Swinters <daan.swinters@gmail.com>
2020-03-19 15:30:41 +0100

    updat usererinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,44 +1,43 @@
-const discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../../functions.js");
 
-module.exports.run = async(bot, message, args) => {
-    var Icon = message.guild.iconURL;
-    var Userroll = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
-    
-    // if (!user.roles) {
-    // user.roles = [ ];
-    // }
-    
-    
-    
-//     let user;
-// if (message.mentions.users.first()) {
-//     user = message.mentions.users.first();
-// } else {
-//     user = message.guild.author;
-// }
+module.exports = {
+    name: "whois",
+    aliases: ["who", "user", "info"],
+    description: "Returns user information",
+    usage: "[username | id | mention]",
+    run: (client, message, args) => {
+        const member = getMember(message, args.join(" "));
 
-const member = message.guild.member(user);
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
 
-const embed = new discord.RichEmbed()
-    .setColor("RANDOM")
-    .setThumbnail(message.author.avatarURL)
-    //.addField(`${user.tag}`, `${user}`, true)
-    .addField("ID:", `${user_id}`)
-    //.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-    //.addField("Status:", `${user.presence.status}`, true)
-    .addField("In Server", message.guild.name, true)
-    //.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-    //.addField("Bot:", `${user.bot}`, true)
-    .addField("Joined The Server On:", message.member.joinedAt)
-    //.addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    //.addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
-    //.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+        // User variables
+        const created = formatDate(member.user.createdAt);
 
+        const embed = new RichEmbed()
+            .setFooter(member.displayName, member.user.displayAvatarURL)
+            .setThumbnail(member.user.displayAvatarURL)
+            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 
-    return message.channel.send(embed);
-}
+            .addField('Member information:', stripIndents`**> Display name:** ${member.displayName}
+            **> Joined at:** ${joined}
+            **> Roles:** ${roles}`, true)
 
-module.exports.help = {
-    name: "userinfo",
-    description: "for see the info from the user"
+            .addField('User information:', stripIndents`**> ID:** ${member.user.id}
+            **> Username**: ${member.user.username}
+            **> Tag**: ${member.user.tag}
+            **> Created at**: ${created}`, true)
+            
+            .setTimestamp()
+
+        if (member.user.presence.game) 
+            embed.addField('Currently playing', stripIndents`**> Name:** ${member.user.presence.game.name}`);
+
+        message.channel.send(embed);
+    }
 }
