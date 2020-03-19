9892625475441dbae24571c9657a3bfa1b8bc865
Daan Swinters <daan.swinters@gmail.com>
2020-03-19 12:45:56 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -22,7 +22,7 @@ const member = message.guild.member(user);
 const embed = new discord.RichEmbed()
     .setColor("RANDOM")
     .setThumbnail(message.author.avatarURL)
-    .addField(`${user.tag}`, `${user}`, true)
+    //.addField(`${user.tag}`, `${user}`, true)
     .addField("ID:", `${user.id}`, true)
     .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
     .addField("Status:", `${user.presence.status}`, true)
@@ -31,7 +31,7 @@ const embed = new discord.RichEmbed()
     .addField("Bot:", `${user.bot}`, true)
     .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
     .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
+    .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
     .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
 
 
