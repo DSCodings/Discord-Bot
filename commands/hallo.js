cf3a1f530590b817db8048f260223368648ac362
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-29 08:57:44 +0200

    Create hallo.js

---
+++ commands/hallo.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    return message.channel.send("Hello");
+}
+
+module.exports.help = {
+    name: "hello",
+    description: "for say hello."
+}
