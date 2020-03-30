6cae049e40da076c1838efbdbbc0ff15ccfce394
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-30 14:35:17 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,10 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    let user = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
-    if (!user) return message.channel.send("give a text");
+    var reason = args.join(" ").slice(22);
+    if (!reason) return message.channel.send("give a text");
     message.channel.bulkDelete(1)
-    message.channel.send(user)
+    message.channel.send(reason)
 
 }
 
