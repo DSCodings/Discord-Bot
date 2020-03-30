a8b051f3d7237d6a902a6ac25006c4ec6f206a09
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-30 14:45:06 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
     var reason = args.join(" ");
     console.log(reason)
-    if (!reason) return message.channel.send("give a text");
+    //if (!reason) return message.channel.send("give a text");
     message.channel.bulkDelete(1)
     message.channel.send(reason)
 
