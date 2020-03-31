630ba7f307c3fee8964875a6d532d853fdb7dda7
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-03-31 15:48:58 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,9 @@
 const discord = require("discord.js");
 
-module.exports.run = async(bot, message, args) => {
-    return message.channel.send("Test is suc6vol gelukt!");
-}
-
-module.exports.help = {
-    name: "test",
-    description: "for test the bot"
-}
+module.exports = {
+	name: 'test',
+	description: 'test!',
+	execute(message, args) {
+		message.channel.send('TOP');
+	},
+};
