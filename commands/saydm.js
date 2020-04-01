dfe2324002f7a6f25bcbb9e748739ac0f1753a61
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-01 12:07:12 +0200

    Update saydm.js

--- commands/saydm.js
+++ commands/saydm.js
@@ -1,12 +1,13 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
+    var user124 = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permission for that.");
     var reason = args.join(" ");
     console.log(reason);
     //if (!reason) return message.channel.send("give a text");
     message.channel.bulkDelete(1);
-    message.author.send(reason);
+    message.user124.send(reason);
 
 }
 
