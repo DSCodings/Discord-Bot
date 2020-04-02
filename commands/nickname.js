325a6d67ac8834c83f1a7bc0a02f71a5b670d215
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-02 15:19:48 +0200

    Update nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -24,9 +24,18 @@ module.exports.run = async(bot, message, args) => {
       .setFooter('Mady by jan0de0man', message.guild.iconURL);
 
 
-    return message.channel.send(nicknameEmbed);
+    message.channel.send(nicknameEmbed);
+    var nicknameEmbed1 =  new discord.RichEmbed()
+      .setDescription("nick name successful changed")
+      .setColor("#29e53f")
+      .setThumbnail(message.guild.iconURL)
+      .addField("nickname successful changed to:", nickname)
+      .setTimestamp()
+      .setFooter('Mady by jan0de0man', message.guild.iconURL);
 
-
+      let nicknamechannel = channel.guild.channels.find(x => x.name === "logs");
+      if (!nicknamechannel) return console.log("Kan het logs kanaal niet vinden.");
+      nicknamechannel.send(nicknameEmbed1);
 }
 
 module.exports.help = {
