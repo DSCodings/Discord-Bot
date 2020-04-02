a9b74844cc586dc6b8ed4d5b276b396ef668afd7
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-02 09:06:05 +0200

    Create nickname.js

---
+++ commands/nickname.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+
+
+
+    var nickname = args.join(" ");
+ 
+   
+    
+
+    if (!nickname) return message.channel.send("<prefix>nicknamechange (user) (New nickname)");
+    
+    message.guild.members.get(message.author).setNickname(nickname);
+
+    var nicknameEmbed =  new discord.RichEmbed()
+      .setDescription("nick name successful changed")
+      .setColor("#29e53f")
+      .setThumbnail(message.guild.iconURL)
+      .addField("nickname successful changed to:", nickname)
+      .addField("More commands", "Do <prefix>help for more commands")
+      .setTimestamp()
+      .setFooter('Mady by jan0de0man', message.guild.iconURL);
+
+
+    return message.channel.send(nicknameEmbed);
+
+
+}
+
+module.exports.help = {
+    name: "verify",
+    description: "for change the nickname af a player"
+}
