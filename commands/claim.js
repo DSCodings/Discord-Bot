47a1ba8c23b3a59b05e6333b950034676e227c62
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-08 10:21:19 +0200

    Create claim.js

---
+++ commands/claim.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    const categoryId = "667393377020411916";
+
+    if (message.channel.parentID == categoryId) {
+ 
+        message.channel.send("This ticket has claimed by:" = message.author.tag)
+ 
+    } else {
+ 
+        message.channel.send("This is not a ticket.");
+ 
+    }
+}
+
+module.exports.help = {
+    name: "claim",
+    description: "for claim a ticket."
+}
