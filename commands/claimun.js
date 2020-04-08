5e6d897ed7dffa09d60034edbf5ecd483692e02a
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-08 10:30:52 +0200

    Create claimun.js

---
+++ commands/claimun.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    const categoryId = "667393377020411916";
+    message.channel.bulkDelete(1);
+    if (message.channel.parentID == categoryId) {
+ 
+        message.channel.send("This ticket has unclaimed by: " + message.author.tag)
+ 
+    } else {
+ 
+        message.channel.send("This is not a ticket.");
+ 
+    }
+}
+
+module.exports.help = {
+    name: "unclaim",
+    description: "for claim a ticket."
+}
