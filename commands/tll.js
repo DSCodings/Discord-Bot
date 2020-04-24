411eb0caeed3457bda7737a48a8f0a3cdc220ae7
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-24 14:16:19 +0200

    Update tll.js

--- commands/tll.js
+++ commands/tll.js
@@ -6,6 +6,7 @@ module.exports.run = async(bot, message, args) => {
     if (!args[0]) return message.channel.send("Geef een argument mee");
     if (!args[1]) return message.channel.send("Geef een argument mee");
     var count = 10 + 10;
+    message.channel.send(count);
     message.channel.send("testendingtesten");
     return message.channel.send(first + ars[1]);
     
