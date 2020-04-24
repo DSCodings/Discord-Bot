4a1ec86390afdfd52a54c7f22baee4486bcb036b
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-24 17:50:50 +0200

    Update tll.js

--- commands/tll.js
+++ commands/tll.js
@@ -1,18 +1,51 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    message.channel.send(count);
-    //var first = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
-    if (!args[0]) return message.channel.send("Geef een argument mee");
-    if (!args[1]) return message.channel.send("Geef een argument mee");
-    var second = args[1]
-    var firsssst = args[0]
-    var count = 10 + 10;
-    message.channel.send(count);
-    message.channel.send("testendingtesten");
-    var somsin = `${firsssst} + ${second}`
-    var somsin22 = somsin
-    return message.channel.send(somsin22);
+    // message.channel.send(count);
+    // //var first = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
+    // if (!args[0]) return message.channel.send("Geef een argument mee");
+    // if (!args[1]) return message.channel.send("Geef een argument mee");
+    // var second = args[1]
+    // var firsssst = args[0]
+    // var count = 10 + 10;
+    // message.channel.send(count);
+    // message.channel.send("testendingtesten");
+    // var somsin = `${firsssst} + ${second}`
+    // var somsin22 = somsin
+    // return message.channel.send(somsin22);
+
+    if (args[0] == "1"){
+        message.channel.send("1");
+
+    }
+    else if (args[0] == "2"){
+        
+    }
+    else if (args[0] == "3"){
+        
+    }
+    else if (args[0] == "4"){
+        
+    }
+    else if (args[0] == "5"){
+        
+    }
+    else if (args[0] == "6"){
+        
+    }
+    else if (args[0] == "7"){
+        
+    }
+    else if (args[0] == "8"){
+        
+    }
+    else if (args[0] == "9"){
+        
+    }
+    else if (args[0] == "10"){
+        
+    }
+    
     
 }
 
