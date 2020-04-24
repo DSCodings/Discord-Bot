177e8e9b65a12af0b01ff1452d40dd5f6fe50a9d
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-24 14:03:56 +0200

    create tll.js

--- commands/claimun.js
+++ commands/claimun.js
@@ -16,5 +16,5 @@ module.exports.run = async(bot, message, args) => {
 
 module.exports.help = {
     name: "unclaim",
-    description: "for claim a ticket."
+    description: "for unclaim a ticket."
 }
---
+++ commands/tll.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    return message.channel.send(ars[0] + ars[0]);
+}
+
+module.exports.help = {
+    name: "test",
+    description: "for test the bot"
+}
