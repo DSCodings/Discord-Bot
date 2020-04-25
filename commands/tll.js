1a4e4e320498117872b4b07497fee2269db362f5
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-04-25 09:46:22 +0200

    Update tll.js

--- commands/tll.js
+++ commands/tll.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    message.channel.send(count);
+    //message.channel.send(count);
     //var first = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
     // if (!args[0]) return message.channel.send("Geef een argument mee");
     // if (!args[1]) return message.channel.send("Geef een argument mee");
