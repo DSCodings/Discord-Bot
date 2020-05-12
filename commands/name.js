27d256f7543d478b30b4cf52bf8b061650b024f3
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-05-12 11:51:33 +0200

    Create name.js

---
+++ commands/name.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    message.guild.fetchAuditLogs({type: 'MEMBER_UPDATE', user: 'DESIRED USER ID'}).then(async (audit) => {
+        let log = audit.entries.first().changes
+        console.log(log)
+    })
+
+module.exports.help = {
+    name: "names",
+    description: "for see the history of your names"
+}
