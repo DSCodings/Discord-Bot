1b35a9aefa0fe83b9912d12841a7c35dc9051266
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-10-04 11:45:20 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -13,9 +13,14 @@ module.exports.run = async(bot, message, args) => {
       .addField("Bot name", bot.user.username)
       .addField("you are joined on", message.member.joinedAt)
       .addField("members", message.guild.memberCount)
+      .addField("Bots", message.guild.members.filter(member => member.user.bot).size)
       .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
       .addField("Your name:", `${message.author.username}#${message.author.discriminator}`)
       .addField("Your id:", message.author.id)
+      .addField("Channals", message.guild.channels.size)
+      .addField("roles", message.guild.roles.size)
+      .addField("Verification Level", verifLevels[message.guild.verificationLevel])
+      .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`)
       .setTimestamp()
       .setFooter('Mady by jan0de0man', message.guild.iconURL);
 
