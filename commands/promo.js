1ddfafba2a3c8495191e02ae6c959477983a58a6
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-10-11 14:50:26 +0200

    Update promo.js

--- commands/promo.js
+++ commands/promo.js
@@ -4,16 +4,32 @@ module.exports.run = async(bot, message, args) => {
 
 
     var Userroll = message.guild.member(message.mentions.users.first() || message.guild.member(args[0]));
-    if(!Userroll) return message.reply("Couldn't find that user!");
-    
-    var muteRole = message.guild.roles.find("name", "Muted");
+    if(!Userroll) return message.reply("gebruiken niet gevonden.");
 
+    if (!args[1]) return message.reply("Geef een rijbewijs mee, A, B, C of D.")
+    
+    var muteRole = message.guild.roles.find("name", "Legale Rijder!");
+    if (!args[1] == 'A'){
+        message.reply("AAA")
+    }
+    else  if (!args[1] == 'B'){
+        message.reply("BBBB")
+    }
+    else  if (!args[1] == 'C'){
+        message.reply("CCCC")
+    }
+    else  if (!args[1] == 'D'){
+        message.reply("DDDD")
+    }
+    var muteRole1 = message.guild.roles.find("name", "Legale Rijder!");
+
+    Userroll.addRole(muteRole);
     Userroll.addRole(muteRole);
     
     var stringEmbed = new discord.RichEmbed()
       .setColor("#ff0000")
       .setTitle("**Promotie!**")
-      .setDescription(`${args[0]} heeft suc6 vol de rank legale rijder gekregen.`)
+      .setDescription(`${args[0]} heeft suc6 vol de Rijbewijs ${args[1]} gekregen.`)
       .setTimestamp()
       .setFooter('Mady by jan0de0man', message.guild.iconURL);
     
