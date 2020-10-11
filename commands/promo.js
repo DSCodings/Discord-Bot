8bed102a3b07962d724b84d579974a6faadfd05f
Jan140900 <62025793+Jan140900@users.noreply.github.com>
2020-10-11 14:27:46 +0200

    Update promo.js

--- commands/promo.js
+++ commands/promo.js
@@ -2,8 +2,16 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
     if (!message.authorh.roles.has == "Peepsman Founders") {
-        return message.reply("Je hebt de rol niet.")
+        return message.reply("Je hebt de rol niet.");
     }
+    message.reply("succes");
+   // let Grole = message.mentions.roles.first();
+
+    
+   // if(!Grole) return message.reply("Couldn't find that role.");
+
+   // Userroll.addRole(Grole);
+
     
 
    
